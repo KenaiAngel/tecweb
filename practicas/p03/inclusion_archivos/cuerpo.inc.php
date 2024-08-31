ea610ebfca081e5f0bc941a011bf70de0d7b6ffd
KenaiAngel <angel.sanchezroj@alumno.buap.mx>
2024-08-30 20:22:24 -0600

    Práctica 3 – Configuración de Apache y PHP

---
+++ practicas/p03/inclusion_archivos/cuerpo.inc.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1> Este es el cuerpo del documento </h1>";
+echo "<h2> Este es el cuerpo del documento </h2>";
---
+++ practicas/p03/inclusion_archivos/principal.php
@@ -0,0 +1,6 @@
+<?php
+include("encabezado.inc.php");
+echo "<hr />";
+include_once("cuerpo.inc.php");
+require("cuerpo.html");
+require_once("pie.inc.php");
