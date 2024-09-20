484e873096c7655a861947201e9beeb7e4e8144f
KenaiAngel <angel.sanchezroj@alumno.buap.mx>
2024-09-20 14:35:44 -0600

    Utiliza un ciclo while/dowhile para encontrar el primer número entero obtenido aleatoriamente, pero que además sea múltiplo de un número dado.

--- practicas/p06/_index.php
+++ practicas/p06/_index.php
@@ -42,14 +42,82 @@
     ?>
 
     <div>
+        
+        <?php
+
+            echo "
+                <h2>
+                    Ejercicio 2
+                </h2>
+            <br>";
+
+
+            $resultado = numAleatorios();
+            $GLOBALS["matriz"] = $resultado[0];
+            $GLOBALS["iteraciones"] = $resultado[1];
+
+            echo "<table border='1' cellspacing='0' cellpadding='5'>";
+            echo "
+            <thead>
+                    <tr>
+                        <th style='color:blue;'>IMPAR</th>
+                        <th style='color:red;'>PAR</th>
+                        <th style='color:blue;'>IMPAR</th>
+                    </tr>
+            </thead>
+            ";
+        
+            echo "<tbody>";
+
+            for ($i = 0; $i < $iteraciones; $i++) {
+                echo "<tr>";
+                for ($j = 0; $j < 3; $j++) {
+                    if ($i == $iteraciones - 1) {
+                        // Colorear la última fila con diferentes estilos
+                        echo "<td style='color:blue;'>" . $matriz[$i][$j] . "</td>";
+                        echo "<td style='color:red;'>" . $matriz[$i][$j+1] . "</td>";
+                        echo "<td style='color:blue;'>" . $matriz[$i][$j+2] . "</td>";
+
+                        break;
+                    } else {
+                        // Mostrar los números normales en otras filas
+                        echo "<td>" . $matriz[$i][$j] . "</td>";
+                    }
+                }
+                echo "</tr>";
+            }
+
+            // Cerrar la tabla y mostrar el número total de números obtenidos
+            echo "</tbody>";
+            echo "</table>";
+
+            echo "<p>" . ($iteraciones * 3) . " números obtenidos en $iteraciones iteraciones</p>";
+
+
+    
+        ?>
+
+    </div>
+
+    <div>
         <h2>
-            Ejercicio 2
+            Ejercicio 3
         </h2>
         <br>
         <?php
-            
-            generarSecuencia();
+         
+            if (isset($_GET['divisor_get'])){
+                $divisor = $_GET['divisor_get'];
+                list($encontrado,$iteraciones) = aleatorioWhile($divisor);
+                list($encontradoDoWhile,$iteracionesDoWhile) = aleatorioDoWhile($divisor);
+                echo "Num $encontrado en $iteraciones iteraciones <br>";
+                echo "Num $encontradoDoWhile en $iteracionesDoWhile iteraciones <br>";
+            }
+
+
         ?>
     </div>
+
+
 </body>
 </html>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -4,6 +4,12 @@ function esMultiploDe5y7($num) {
     return ($num % 5 == 0 && $num % 7 == 0);
 }
 
+function esMultiploDe($dividendo, $divisor) {
+    return ($dividendo % $divisor == 0 );
+}
+
+
+
 function esImpar($num) {
     return $num % 2 != 0;
 }
@@ -11,7 +17,8 @@ function esImpar($num) {
 function esPar($num) {
     return $num % 2 == 0;
 }
-/*
+
+
 function numAleatorios() {
     $secuencias = [];
     $iteraciones = 0;
@@ -25,23 +32,64 @@ function numAleatorios() {
         $num3 = rand(1, 1000);
 
         $secuencias[] = [$num1, $num2, $num3];
-        echo "  ".$num1."    ".$num2."   ".$num3."   <br>";
 
         if (esImpar($num1) && esPar($num2) && esImpar($num3)) {
-            echo ($iteraciones*3)." números obtenidos en ".$iteraciones." iteraciones".'<br>';
+            
             break;
         } 
 
     } while (true);
 
-}*/
-function generarSecuencia() {
+    return [$secuencias, $iteraciones];
+
+}
+
+function aleatorioWhile($divisor){
+    $iteraciones =0;
+    $encontrado = false;
+
+    while(!$encontrado){
+        $num1 = rand(1, 1000);
+        $iteraciones++;
+        if($num1 % $divisor == 0){
+            $encontrado = true;
+        }
+
+    }
+    return [$num1,$iteraciones];
+}
+
+
+function aleatorioDoWhile($divisor){
+    $iteraciones =0;
+
+
+    do{
+        $num1 = rand(1, 1000);
+        $iteraciones++;
+
+    }while($num1 % $divisor != 0);
+
+    return [$num1,$iteraciones];
+}
+
+/*
+function numAleatorios() {
     $secuencias = [];
     $iteraciones = 0;
 
 
     echo "<table border='1' cellspacing='0' cellpadding='5'>";
-    echo "<thead><tr><th>IMPAR</th><th>PAR</th><th>IMPAR</th></tr></thead>";
+    echo "
+    <thead>
+            <tr>
+                <th style='color:blue;'>IMPAR</th>
+                <th style='color:red;'>PAR</th>
+                <th style='color:blue;'>IMPAR</th>
+            </tr>
+    </thead>
+    ";
+
     echo "<tbody>";
 
     do {
@@ -53,25 +101,31 @@ function generarSecuencia() {
 
         $secuencias[] = [$num1, $num2, $num3];
 
-
-        echo "<tr>";
-        echo "<td>$num1</td>";
-        echo "<td>$num2</td>";
-        echo "<td>$num3</td>";
-        echo "</tr>";
-
    
         if (esImpar($num1) && esPar($num2) && esImpar($num3)) {
-            
+
+            echo "<tr>";
+            echo "<td style='color:blue;' >$num1</td>";
+            echo "<td style='color:red;' >$num2</td>";
+            echo "<td style='color:blue;' >$num3</td>";
+            echo "</tr>";
+
             echo "</tbody>";
             echo "</table>";
 
             echo "<p>" . ($iteraciones * 3) . " números obtenidos en $iteraciones iteraciones</p>";
             break;
         }
+        else{
+            echo "<tr>";
+            echo "<td>$num1</td>";
+            echo "<td>$num2</td>";
+            echo "<td>$num3</td>";
+            echo "</tr>";
+        }
 
     } while (true);
 }
 
-
+*/
 ?>
