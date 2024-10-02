09ac7a783a442bc3195a75b4c3da554ff7523bf4
KenaiAngel <angel.sanchezroj@alumno.buap.mx>
2024-10-02 00:41:25 -0600

    Tarea XHTML:- Validador como prueba de que el documento cumple todas las reglas de XHTML.

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -140,5 +140,9 @@ c. El idioma del navegador (cliente). */
 
 
     ?>
+    <p>
+    <a href="https://validator.w3.org/markup/check?uri=referer"><img
+      src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
 </body>
 </html>
